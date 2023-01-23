129d2e98d695ed92394be567cff83bda7b32f67a
Hamama Assadi <hmima495gmail.com>
2023-01-23 22:30:58 +0100

    projet API

--- routes/sauces.js
+++ routes/sauces.js
@@ -1,24 +1,25 @@
 //création d'un router qu'on va importer dans l'app
 const express = require("express");
-const auth = require('auth');
+const auth = require('../middleware/auth');
+
 const router = express.Router();
 
 const SaucesCtrl = require('../controllers/sauces');
 
 /* route qui permet d'afficher les articles*/
-router.get("/", SaucesCtrl.getAllSauces);
+router.get("/", auth, SaucesCtrl.getAllSauces);
 
 /*routes pr recevoir données via la console car pas encore de base de données+on voit ce qu'elles font: crer, modifier etc)*/
-router.post('/', SaucesCtrl.createSauce);
+router.post('/', auth,  SaucesCtrl.createSauce);
 
 //La méthode  app.get()  permet de réagir uniquement aux requêtes de type GET.
-router.get("/:id", SaucesCtrl.getOneSauce);
+router.get("/:id", auth, SaucesCtrl.getOneSauce);
 
 //Methode put pour mettre à jour une sauce
-router.put("/:id", SaucesCtrl.modifySauce);
+router.put("/:id", auth, SaucesCtrl.modifySauce);
 
 //Route pour supprimer un objet
-router.delete("/:id", SaucesCtrl.deleteSauce);
+router.delete("/:id", auth, SaucesCtrl.deleteSauce);
 
 
 
